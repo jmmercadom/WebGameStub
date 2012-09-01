92d0933b7d09e8b59014f1199b5545013e7f88ce
Jesus Mercado <jesusmmm@gmail.com>
2012-09-01 14:38:02 -0300

    Add image backgroung

--- www/js/app.js
+++ www/js/app.js
@@ -9,55 +9,66 @@
 // Set the path to jQuery, which will fall back to the local version
 // if google is down
 require.config({
-  baseUrl: 'js/lib',
-  paths: {
-    'jquery':
-            ['//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
-             'lib/jquery']
-  }
+	baseUrl: 'js/lib',
+paths: {
+	'jquery':
+	['//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
+'lib/jquery']
+}
 });
 
 var global = this;
 
 require(['jquery'], function($) {
-  // Create the canvas
-  var canvas = document.createElement("canvas");
-  var ctx = canvas.getContext("2d");
-  canvas.width = 512;
-  canvas.height = 480;
-  document.body.appendChild(canvas);
+	// Create the canvas
+	var canvas = document.createElement("canvas");
+	var ctx = canvas.getContext("2d");
+	canvas.width = 700;
+	canvas.height = 575;
+	document.body.appendChild(canvas);
 
-//Reset game to original state
-  function reset() {
+	//Background image
+	var bgReady = false;
+	var bgImage = new Image();
+	bgImage.onload = function() {
+		bgReady = true;
+	};
+	bgImage.src = "img/background.png";
 
-  };
+	//Reset game to original state
+	function reset() {
 
-// Update game objects
-  function update( modifier ) {
+	};
 
-  };
+	// Update game objects
+	function update( modifier ) {
 
-// Draw everything
-  function render() {
+	};
 
-  };
+	// Draw everything
+	function render() {
+		if(bgReady) {
+			ctx.drawImage(bgImage, 0, 0);
+		}
 
-// The main game loop
-  var main = function () {
-    var now = Date.now();
-    var delta = now - then;
+	};
 
-    update(delta / 1000);
-    render();
+	// The main game loop
+	var main = function () {
+		var now = Date.now();
+		var delta = now - then;
 
-    then = now;
-  };
+		update(delta / 1000);
+		render();
 
-// Let's play this game!
-  reset();
-  var then = Date.now();
-  // Execute as fast as possible
-  setInterval(main, 1);
+		then = now;
+	};
+
+	// Let's play this game!
+	reset();
+	var then = Date.now();
+	// Execute as fast as possible
+	setInterval(main, 1);
 
 });
 
